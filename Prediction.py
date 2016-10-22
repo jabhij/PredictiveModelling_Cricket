082646edf187df0c7c4ee2722790c0d9c390d99c
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-10-22 12:20:47 +0530

    Prediction based on Naive Bayes Model

---
+++ Prediction.py
@@ -0,0 +1,37 @@
+"""
+    Labels : Lost, Draw, Won [-1,0,1]
+
+    Features --
+    ==========
+        Toss(Lost,Won) = [-1,1]
+        Bat(First, Second) = [-1,1]
+"""
+
+# Import Library - Gaussian Naive Bayes model
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+# Import Numpy
+import numpy as np
+from sklearn.metrics import precision_recall_fscore_support as score
+
+
+# Assigning Features
+features = np.genfromtxt('train.csv',delimiter=',',usecols=(1,2),dtype=int)
+labels = np.genfromtxt('train.csv',delimiter=',',usecols=(0),dtype=int)
+
+features_test = np.genfromtxt('test.csv',delimiter=',',usecols=(1,2),dtype=int)
+labels_test = np.genfromtxt('test.csv',delimiter=',',usecols=(0),dtype=int)
+
+# Create a Gaussian Classifier
+model = GaussianNB()
+
+# Train the model using the training sets
+model.fit(features, labels)
+
+# Prediction
+predicted = model.predict(features_test)
+# print(predicted)
+
+acc = accuracy_score(labels_test,predicted)
+print(acc)
